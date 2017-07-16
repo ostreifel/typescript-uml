fe822b468c94b4837d8d366c11ee5c68531ccc3b
Otto Streifel <ottost@microsoft.com>
2017-07-16 11:11:26 -0700

    breakup diagram code

--- src/view/diagram.ts
+++ src/view/diagram.ts
@@ -1,50 +1,53 @@
 
-function run() {
+function getStyle(): any {
+    // tslint:disable-next-line:no-string-literal
+    return cytoscape["stylesheet"]()
+        .selector("node")
+        .css({
+            "background-color": "blue",
+            "color": "white",
+            "content": "data(name)",
+            "text-outline-color": "black",
+            "text-outline-width": 2,
+            "text-valign": "center",
+        })
+        .selector("edge")
+        .css({
+            "curve-style": "bezier",
+            "line-color": "#ccc",
+            "target-arrow-color": "#ccc",
+            "target-arrow-shape": "triangle",
+            "width": "data(weight)",
+        })
+        .selector(":selected")
+        .css({
+            "background-color": "black",
+            "line-color": "black",
+            "source-arrow-color": "black",
+            "target-arrow-color": "black",
+        })
+        .selector(".faded")
+        .css({
+            "opacity": 0.25,
+            "text-opacity": 0,
+        });
+}
+function coseLayout(): Cy.CoseLayoutOptions {
     const layout = {
-            name: "cose",
-            numIter: 10000,
-            randomize: false,
+        name: "cose",
+        numIter: 10000,
+        randomize: false,
     } as Cy.CoseLayoutOptions;
+    return layout;
+}
+function run() {
     const cy = cytoscape({
         container: $("#cy"),
         elements: JSON.parse($("#models").html()),
-
         autounselectify: true,
         boxSelectionEnabled: false,
-
-        // tslint:disable-next-line:no-string-literal
-        style: cytoscape["stylesheet"]()
-            .selector("node")
-            .css({
-                "background-color": "blue",
-                "color": "white",
-                "content": "data(name)",
-                "text-outline-color": "black",
-                "text-outline-width": 2,
-                "text-valign": "center",
-            })
-            .selector("edge")
-            .css({
-                "curve-style": "bezier",
-                "line-color": "#ccc",
-                "target-arrow-color": "#ccc",
-                "target-arrow-shape": "triangle",
-                "width": "data(weight)",
-            })
-            .selector(":selected")
-            .css({
-                "background-color": "black",
-                "line-color": "black",
-                "source-arrow-color": "black",
-                "target-arrow-color": "black",
-            })
-            .selector(".faded")
-            .css({
-                "opacity": 0.25,
-                "text-opacity": 0,
-            }),
-
-        layout,
+        style: getStyle(),
+        layout: coseLayout(),
 
     });
 }
