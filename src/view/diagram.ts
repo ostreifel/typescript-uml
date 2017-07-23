a24d00c203d24f12c77ebc7628f3946ecfe9210c
Otto Streifel <ottost@microsoft.com>
2017-07-22 18:25:23 -0700

    respect class/enum/interface borders when creating layout

--- src/view/diagram.ts
+++ src/view/diagram.ts
@@ -34,6 +34,103 @@ function getStyle(): any {
         });
 }
 
+interface IGridPos {
+    col: number;
+    row: number;
+}
+class BoxGridLayout {
+    private readonly elementCount: number;
+    private readonly parented: { [parentId: string]: Cy.NodeCollection } = {};
+    private readonly unparented: Cy.NodeCollection;
+    private readonly positions: {[id: string]: IGridPos} = {};
+    private readonly posGrid: string[][] = [];
+
+    constructor(
+        private readonly cy: Cy.Core,
+        eles: Cy.NodeCollection,
+    ) {
+        this.elementCount = getNodes(eles, (element) => !element.isParent()).length;
+        eles.nodes(":parent").map((p) => p.id()).forEach((parentId) => {
+            this.parented[parentId] = getNodes(eles, (element: Cy.NodeSingular) => {
+                return parentId === element.data("parent");
+            });
+        });
+        this.unparented = getNodes(eles, (element: Cy.NodeSingular) => {
+            return !element.data("parent") && !element.isParent();
+        });
+        this.calcPositions();
+    }
+
+    public getLayout(): Cy.GridLayoutOptions {
+        return {
+            name: "grid",
+            position: this.position.bind(this),
+            rows: this.maxRow(),
+            cols: this.maxCol(),
+            // nodeDimensionsIncludeLabels: true,
+            condense: true,
+            avoidOverlapPadding: 140,
+        } as Cy.GridLayoutOptions;
+    }
+
+    private position(node: Cy.NodeSingular): IGridPos {
+        return this.positions[node.id()];
+    }
+
+    private maxRow() {
+        return this.posGrid.length;
+    }
+    private maxCol() {
+        let maxCol = 0;
+        for (let i = 0; i < this.posGrid.length; i++) {
+            if (this.posGrid[i] && this.posGrid[i].length > maxCol) {
+                maxCol = this.posGrid[i].length;
+            }
+        }
+        return maxCol;
+    }
+
+    private getRow(i: number) {
+        if (!this.posGrid[i]) {
+            this.posGrid[i] = [];
+        }
+        return this.posGrid[i];
+    }
+
+    private calcPositionsFor(eles: Cy.NodeCollection, startCol: number, startRow: number): void {
+        const wrapThreshold = Math.ceil(Math.sqrt(eles.length));
+        let col = 0;
+        let row = 0;
+        function next() {
+            col++;
+            if (col > wrapThreshold) {
+                col = 0;
+                row++;
+            }
+        }
+        for (let i = 0; i < eles.length; i++) {
+            this.getRow(row + startRow)[col + startCol] = eles[i].id();
+            next();
+        }
+    }
+    private calcPositions() {
+        const wrapThreshold = Math.ceil(Math.sqrt(this.elementCount));
+        this.calcPositionsFor(this.unparented, 0, 0);
+        for (const parentId in this.parented) {
+            this.calcPositionsFor(this.parented[parentId], 0, this.posGrid.length);
+        }
+        for (let i = 0; i < this.posGrid.length; i++) {
+            const row = this.getRow(i);
+            for (let j = 0; j < row.length; j++) {
+                if (row[j]) {
+                    this.positions[row[j]] = {row: i, col: j};
+                }
+            }
+        }
+    }
+}
+
+// tslint:disable-next-line:max-classes-per-file
 class Layouts {
     public static coseLayout(): Cy.CoseLayoutOptions {
         const layout = {
@@ -65,8 +162,8 @@ class Layouts {
         return layout;
     }
 }
-function getNodes(cy: Cy.Core, filter: (n: Cy.NodeSingular) => boolean) {
-    return cy.nodes(filter as any);
+function getNodes(eles: Cy.NodeCollection, filter: (n: Cy.NodeSingular) => boolean) {
+    return eles.nodes(filter as any);
 }
 function applyLayout(nodes: Cy.NodeCollection, layoutOptions: Cy.LayoutOptions) {
     const layout: Cy.Layouts = nodes.layout(layoutOptions) as any;
@@ -82,18 +179,8 @@ function run() {
         style: getStyle(),
         layout: {name: "null"} as Cy.NullLayoutOptions,
     });
-    const parentIds: {[parentId: string]: void} = {};
-    elements.map((e) => e.data["parent"]).filter((p) => p).forEach((p) => parentIds[p] = undefined);
-    for (const parentId of Object.keys(parentIds)) {
-        const children = getNodes(cy, (element: Cy.NodeSingular) => {
-            return parentId === element.data("parent");
-        });
-        applyLayout(children, Layouts.gridLayout());
-    }
-    const parents = getNodes(cy, (element) => {
-        return !element.data("parent") || (element.id() in parentIds);
-    });
-    applyLayout(parents, Layouts.gridLayout());
-    // TODO more advanced layout here - select neighborhoods
+    const boxLayout = new BoxGridLayout(cy, cy.nodes());
+    applyLayout(cy.nodes(), boxLayout.getLayout());
 }
+// $("#cy").click(run);
 setTimeout(run, 0);
