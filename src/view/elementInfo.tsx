b380ba39f04a167a44a5a411ebbff65f844fb519
Otto Streifel <ottost@microsoft.com>
2017-07-26 17:44:29 -0700

    edges selection

--- src/view/elementInfo.tsx
+++ src/view/elementInfo.tsx
@@ -11,30 +11,42 @@ export function registerInfoPane(
 }
 
 function showElementInfo(e: Cy.EventObject) {
-    const node: Cy.NodeCollection = e.target;
+    const target: Cy.SingularData = e.target;
 
-    if ((e.type as Cy.CollectionEventName) === "select" && node.nonempty() && node.isNode()) {
-        show(node);
+    if ((e.type as Cy.CollectionEventName) === "select" && target.isNode()) {
+        if (target.isNode()) {
+            showNode(target as Cy.NodeSingular);
+        } else if (target.isEdge) {
+            showEdge(target as Cy.EdgeSingular);
+        }
     } else {
         hide();
     }
 }
 
-function show(node: Cy.NodeSingular) {
-    ReactDom.render(<NodeInfo getData={node.data.bind(node)} />, infoElement);
-}
 function hide() {
     infoElement.innerHTML = "";
 }
-
-function focusLine(file: string, line: number, column: number) {
-    spawn("code", ["-g", `${file}:${line}:${column}`]);
+function showEdge(edge: Cy.EdgeSingular) {
+    ReactDom.render(<EdgeInfo getData={edge.data.bind(edge)} />, infoElement);
 }
-
-class NodeInfo extends React.Component<{getData: (key: string) => any}, {}> {
+class EdgeInfo extends React.Component<{ getData: (key: string) => any }, {}> {
     public render() {
         const { getData } = this.props;
-        return <div className="node-info">
+        return <div className="edge">
+            <div>{`${getData("weight")} references`}</div>
+        </div>;
+    }
+}
+
+function showNode(node: Cy.NodeSingular) {
+    ReactDom.render(<NodeInfo getData={node.data.bind(node)} />, infoElement);
+}
+// tslint:disable-next-line:max-classes-per-file
+class NodeInfo extends React.Component<{ getData: (key: string) => any }, {}> {
+    public render() {
+        const { getData } = this.props;
+        return <div className="node">
             <h1 className="name">{getData("name")}</h1>
             <div className="type">{getData("type")}</div>
             <a className="line" onClick={this.focusLine.bind(this)} href="#">
@@ -50,6 +62,9 @@ class NodeInfo extends React.Component<{getData: (key: string) => any}, {}> {
     }
     private focusLine() {
         const { getData } = this.props;
-        focusLine(getData("fileName"), getData("startLine"), getData("startColumn"));
+        const file = getData("fileName");
+        const line = getData("startLine");
+        const column = getData("startColumn");
+        spawn("code", ["-g", `${file}:${line}:${column}`]);
     }
 }
