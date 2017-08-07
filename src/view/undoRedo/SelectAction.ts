d2b3955504e34b3cd3352a7bba72a08fd17a4195
Otto Streifel <ottost@microsoft.com>
2017-08-06 20:43:12 -0700

    pan on select

--- src/view/undoRedo/SelectAction.ts
+++ src/view/undoRedo/SelectAction.ts
@@ -39,10 +39,35 @@ export class SelectAction extends UndoRedoAction<ISelectAction> {
         if (ele.isNode()) {
             this.addNodeEdgeHighlights(ele as Cy.NodeCollection);
         }
+        this.panIfNecessary(ele);
         ele.selectify();
         ele.select();
         ele.unselectify();
         this.onSelect(ele);
+    }
+    private panIfNecessary(ele: Cy.NodeCollection) {
+        if (this.cy) {
+            const cyExtent = this.cy.extent();
+            const box = ele.boundingBox({}) as Cy.BoundingBox12 & Cy.BoundingBoxWH;
+            let deltaX = 0;
+            let deltaY = 0;
+            if (box.x1 < cyExtent.x1) {
+                deltaX = cyExtent.x1 - box.x1;
+            } else if (box.x2 > cyExtent.x2) {
+                deltaX = cyExtent.x2 - box.x2;
+            }
+            if (box.y1 < cyExtent.y1) {
+                deltaY = cyExtent.y1 - box.y1;
+            } else if (box.y2 > cyExtent.y2) {
+                deltaY = cyExtent.y2 - box.y2;
+            }
+            const boundingRect = (this.cy.container() as Element).getBoundingClientRect();
+
+            // Convert from position units to pixel units
+            deltaX *= boundingRect.width / cyExtent.w;
+            deltaY *= boundingRect.height / cyExtent.h;
+            this.cy.panBy({x: deltaX, y: deltaY});
+        }
     }
     private addNodeEdgeHighlights(node: Cy.NodeCollection) {
         node.incomers().edges("").addClass("incoming");
